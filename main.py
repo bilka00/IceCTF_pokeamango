bc3651420b12d8efeb14f1982f450232f28324e4
bilka00 <bilka00@mail.ru>
2016-08-15 14:48:46 +0300

    Релиз

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ i = 0
 j = 0
 n = 180
 while i < n:
-    i = i + 0.5
+    i = i + 0.4
     while j < n:
-        j = j + 0.5
-        HTTP = requests.post("http://poke-a-mango.vuln.icec.tf/mango/list", data = {"uuid": "121212121238400000-8cf0-11bd-b23e-10b96e4ef00d", "lat":i,"long":j})
+        j = j + 0.4
+        print str(i)+"_"+str(j)
+        HTTP = requests.post("http://poke-a-mango.vuln.icec.tf/mango/list", data = {"uuid": "121212121238400000-8cf0-11bd-b23e-10b96e4ef00d", "lat":j,"long":i})
         parsed1 = json.loads(HTTP.text)
         for text in parsed1["mangos"]:
             print str(text["lat"])+"_"+str(text['lng'])
